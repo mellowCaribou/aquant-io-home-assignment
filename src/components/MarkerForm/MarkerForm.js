0d5f82ae027c50878c15174bc4407ed0d8e5f861
liorEliavAquant <68593008+liorEliavAquant@users.noreply.github.com>
2020-08-12 12:39:18 +0300

    commit

--- src/components/MarkerForm/MarkerForm.js
+++ src/components/MarkerForm/MarkerForm.js
@@ -1,13 +1,40 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect, useRef } from 'react'
 import Button from '@material-ui/core/Button';
 import { MarkersContext } from '../context/MarkersState';
 
 
 export const MarkerForm = ({ className }) => {
 
-    const markers = useContext(MarkersContext);
-    const [lat, setLat] = useState(null)
-    const [long, setLong] = useState(null)
+    const markersContext = useContext(MarkersContext);
+    const [mapName, setMapName] = useState('');
+    const [mapId, setMapId] = useState(null);
+    const [lat, setLat] = useState(null);
+    const [long, setLong] = useState(null);
+    const mapNameInput = useRef(null);
+
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(window.location.search);
+        const mapIdParam = urlParams.get('mapId');
+
+        if(mapIdParam){
+            setMapId(mapIdParam);
+            window.Visualforce.remoting.Manager.invokeAction('MapsController.GetMap', mapIdParam, (res,e) => {
+                console.log('GetMap', res, e);
+                if(mapNameInput.current){
+                    setMapName(res.Name);
+                    mapNameInput.current.value = res.Name;
+                }
+                if(Array.isArray(res.Map_Cords__r)){
+                    res.Map_Cords__r.forEach(map_cord => markersContext.editMarkers({
+                        action: 'add',
+                        marker: {
+                            location: [map_cord.Latitude__c, map_cord.Longitude__c]
+                        }
+                    }))
+                }
+            });
+        }
+    }, []);
 
     const isLatValid = (lat) => {
         return lat <= 90 && lat >= -90;
@@ -16,26 +43,57 @@ export const MarkerForm = ({ className }) => {
     const isLongValid = (long) => {
         return long <= 180 && long >= -180;
     }
-    // we still have a bug happening here
+
+    const markerToCordSObject = (marker) => {
+        return {
+            Longitude__c: marker.location[1],
+            Latitude__c: marker.location[0]
+        }
+    }
+
     const addNewMarker = () => {
         if (!long || !lat || !isLatValid(lat) || !isLongValid(long)) {
             return alert('please insert a valid location');
         }
-        markers.editMarkers({
-            marker: {
-                "location": [lat, long]
-            },
+        const newMarker = {
+            location: [lat, long]
+        }
+        markersContext.editMarkers({
+            marker: newMarker,
             action: 'add'
-        })
+        });
+        saveMap([newMarker]);
     }
 
     const removeMarkerHandler = (marker) => {
         return () => {
-            markers.editMarkers({
+            markersContext.editMarkers({
                 marker: marker,
                 action: 'remove'
+            });
+            saveMap(null, [marker]);
+        };
+    }
+
+    const saveMap = (markersToAdd, markersToDelete) => {
+        markersToAdd = markersToAdd || [];
+        markersToDelete = markersToDelete || [];
+        if(mapId){
+            window.Visualforce.remoting.Manager.invokeAction('MapsController.UpdateMap', mapId, mapName, markersToAdd.map(markerToCordSObject), markersToDelete.map(markerToCordSObject), (res,e) =>{
+                console.log('UpdateMap', res, e)
             })
         }
+        else {
+            window.Visualforce.remoting.Manager.invokeAction('MapsController.CreateMap', mapName, markersContext.markers.map(markerToCordSObject), (res,e) =>{
+                console.log('CreateMap', res, e)
+                setMapId(res);
+            })
+        }
+
+    }
+
+    const onSaveClick = (e) => {
+        saveMap();
     }
 
     return (
@@ -46,8 +104,12 @@ export const MarkerForm = ({ className }) => {
                 <input placeholder="Long" min="-180" max="180" type="number" name="long" onChange={(event) => setLong(event.target.value)}></input>
                 <Button className='add-btn' onClick={addNewMarker}>Add Marker</Button>
             </form>
+            <form className="">
+                <input ref={mapNameInput} placeholder="Map Name" type="text" name="mapName" onChange={(event) => setMapName(event.target.value)}></input>
+                <Button className='add-btn' onClick={onSaveClick}>Save</Button>
+            </form>
             <ul>
-                {markers.markers.map((marker, i) => (<li className='marker-item' key={i}>
+                {markersContext.markers.map((marker, i) => (<li className='marker-item' key={i}>
                 <div>{'Long:' + marker.location[0] + ', Lat:' + marker.location[1]}</div>
                 <Button onClick={removeMarkerHandler(marker)}>Delete</Button>
                 </li>))}
